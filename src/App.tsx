c3428cfed78e9a9deb4134012697bf3fae666e98
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-01-23 00:03:55 -0400

    finalização frontend web

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import './styles/global.css';
+import { Header } from "./Components/Header";
+import { SummaryTable } from "./Components/SummaryTable";
 
+// import { Habit } from "./components/Habit"
 
-function App() {
-
+export function App() {
   return (
-    <h1> Hello World!</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
 
-export default App
-
-
+//componente: Reaproveitar / isolar
+// propriedade: informação enviada pra modificar um componente visual ou comportamentalmente
